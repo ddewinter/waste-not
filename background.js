af2c760d103f61bd555e2dda829a27f8327e79db
ddewinter <david.dewinter@gmail.com>
2014-10-12 14:10:39 -0700

    Log feature ideas.

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
 function onNavigatingToFacebook(details) {
   console.log("onBeforeNavigate: " + details.url);
   
+  // Ideas:
+  //   1. Shouldn't stay up too late or waste time early in the day: Block at certain times of day.
+  //   2. Should discourage one-off distractions:
+  //       a. Delay before access (30s).
+  //       b. Limit total amount of time per day - put a timer on the page.
+  //       c. Large amount of time in between accesses - e.g. 25 minutes
   var tabUpdate = { url: chrome.extension.getURL("replacement.html") };
   console.log("jsRunner: " + tabUpdate.url);
   chrome.tabs.update(details.tabId, tabUpdate);
