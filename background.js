59df8fa6822ce1c94e70b068191782af08b25ec7
ddewinter <david.dewinter@gmail.com>
2014-10-12 14:50:30 -0700

    Blocks multiple Facebook tabs as well as multiple (sequential) sessions within a 25 minute period.

--- background.js
+++ background.js
@@ -8,10 +8,28 @@ function onNavigatingToFacebook(details) {
   //       b. Limit total amount of time per day - put a timer on the page.
   //       c. Large amount of time in between accesses - e.g. 25 minutes
   
-  
-  
-  var tabUpdate = { url: chrome.extension.getURL("replacement.html") };
-  chrome.tabs.update(details.tabId, tabUpdate);
+  chrome.storage.sync.get(
+    "lastSession",
+    function (items) {
+      var lastSession = items.lastSession || {};
+      console.log("Last Session Info. Tab Id: %d. Time: %d", lastSession.tabId, lastSession.time);
+      
+      // Same session; ignore.
+      if (details.tabId === lastSession.tabId) {
+        return;
+      }
+      
+      // If we're not in the same tab and it hasn't been 25 minutes since the last session started.
+      if (details.tabId !== lastSession.tabId && (new Date().getTime() - lastSession.time) < (1000 * 60 * 25)) {
+        var tabUpdate = { url: chrome.extension.getURL("replacement.html") };
+        chrome.tabs.update(details.tabId, tabUpdate);
+        return;
+      }
+      
+      lastSession = { tabId: details.tabId, time: new Date().getTime() };
+      chrome.storage.sync.set({ lastSession: lastSession });
+      console.log("New session detected. Saving info. Tab Id: %d. Time: %d", lastSession.tabId, lastSession.time);
+    });
 }
 
 if (chrome.webNavigation && chrome.webNavigation.onBeforeNavigate) {
@@ -20,8 +38,11 @@ if (chrome.webNavigation && chrome.webNavigation.onBeforeNavigate) {
     {
       url: [ { hostContains: "facebook.com" } ]
     }
-  );
+  );  
 }
 else {
   console.error("Waste Not: WebNavigation not enabled.");
 }
+
+chrome.storage.sync.set({ lastSession: null });
+console.log("Reset last session.");
